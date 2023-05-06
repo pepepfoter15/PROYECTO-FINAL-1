3c783e8ecc87dab379ba5192193c019cdb7f0a89
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-05-06 21:16:44 +0200

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -9,8 +9,7 @@ print("------------------------------------------------------------")
 codigo_jugador = input("Introduce el código sin # del jugador de Clash of Clans: ")
 
 url = F'https://api.clashofclans.com/v1/players/%23{codigo_jugador}'
-token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjgwYTBkMzdlLWM5NDktNGQ4OC05NTUyLTAyMzUyNzQxMjBkNSIsImlhdCI6MTY4MzM4NzM5OSwic3ViIjoiZGV2ZWxvcGVyLzg2ODg2OGE0LTc3YTktNzNkZS1mMmY5LWU1ZTljZWQ4NGI5NiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjkwLjE2Ny45OC41OCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.TsdkgTKkFXZYTQgmg44nBDvegM-UyqXno7IA05qux7w_v8I2XBpQP_T2gVueW0K1LLLH5P07cfIZxLKx_yBCdQ'
-
+token = 'Bearer [INTRODUCE_TU_TOKEN]'
 headers = {'Authorization': token}
 
 response = requests.get(url, headers=headers)
